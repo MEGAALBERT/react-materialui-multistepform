8155ac9b682c738d49d5deac3428d83184c69752
Alberto Medellin <albertomedellin85@gmail.com>
2020-10-21 12:44:49 -0700

    finished app

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Confirm from './Confirm';
 import FormPersonalDetails from './FormPersonalDetails';
 import FormUserDetails from "./FormUserDetails";
+import Success from './Success';
 
 export class UserForm extends Component {
 
@@ -71,7 +72,9 @@ export class UserForm extends Component {
                 />
                 )
             case 4:
-                return <h1>Success</h1>
+                return (
+                    <Success/>
+                )
             default:
                 return null
         }
